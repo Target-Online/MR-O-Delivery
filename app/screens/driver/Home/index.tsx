0e52bae423b99c8bae444cc56b4e19c7ab4c8c9e
Admin <mnotho.mlg@gmail.com>
2020-09-22 14:22:07 +0200

    mock order fix

--- app/screens/driver/Home/index.tsx
+++ app/screens/driver/Home/index.tsx
@@ -376,7 +376,7 @@ class Home extends React.Component<IProps, IState> {
                       "You're offline and won't receive any requests"}
                     </Text>
                   </View>
-                  <Btn
+                  {/* <Btn
                     style={{width : 120,height:46 , justifyContent : "center" , alignItems : "center", backgroundColor : Colors.primaryOrange , borderRadius :3}}
                     onPress={ async () => {
                       sendRequest(`some${randomNum}order${randomNum}`, mockOrder,()=>{},()=>{})
@@ -385,7 +385,7 @@ class Home extends React.Component<IProps, IState> {
                     }}    
                   >
                       <Text style={{color : "#fff"}} > Add Mock Order</Text>
-                  </Btn>
+                  </Btn> */}
               </View>           
             </View>    
       ]
